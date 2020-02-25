209103033de9a4b8515759f957eccd92374b96db
karl <karl.bittner@outlook.com>
2020-02-25 16:48:57 +0100

    Crop image

--- main.cpp
+++ main.cpp
@@ -2,16 +2,14 @@
 #include <gdal/gdal_priv.h>
 #include <gdal/gdalwarper.h>
 
-int main() {
+void reproject_to_webmercator(const std::string &infile, const std::string &outfile) {
     GDALDriverH hDriver;
     GDALDataType eDT;
     GDALDatasetH hDstDS;
     GDALDatasetH hSrcDS;
 
-    GDALAllRegister();
-
     // Open the source file.
-    hSrcDS = GDALOpen("data/25m_EU_clip.tif", GA_ReadOnly);
+    hSrcDS = GDALOpen(infile.c_str(), GA_ReadOnly);
     CPLAssert(hSrcDS != NULL);
 
     // Create output with same datatype as first input band.
@@ -54,35 +52,9 @@ int main() {
 
     //the affine transformation information, you will need to adjust this to properly
     //display the clipped raster
-    double transform[6];
-    GDALGetGeoTransform(hSrcDS, transform);
-
-    double new_top_left_x = 4563262.0;
-    double new_top_left_y = 2707690.0;
-    double new_size = 1000.0;
-
-    double x_diff = new_top_left_x - transform[0];
-    double y_diff = new_top_left_y - transform[3];
-
-    adfDstGeoTransform[0] += x_diff;
-    adfDstGeoTransform[3] += y_diff;
-
-    std::cout << transform[0] << std::endl;
-    std::cout << transform[3] << std::endl;
-
-    //adjust top left coordinates
-    transform[0] = new_top_left_x;
-    transform[3] = new_top_left_y;
-
-    //determine dimensions of the new (cropped) raster in cells
-    int xSize = round(new_size/transform[1]);
-    int ySize = round(new_size/transform[1]);
-
-    std::cout << nPixels << std::endl;
-    std::cout << nLines << std::endl;
 
     // Create the output file.
-    hDstDS = GDALCreate(hDriver, "data/out2.tif", nPixels / 4.0, nLines / 4.0,
+    hDstDS = GDALCreate(hDriver, outfile.c_str(), nPixels, nLines,
                         GDALGetRasterCount(hSrcDS), eDT, NULL);
     CPLAssert(hDstDS != NULL);
 
@@ -128,6 +100,98 @@ int main() {
     GDALDestroyWarpOptions(psWarpOptions);
     GDALClose(hDstDS);
     GDALClose(hSrcDS);
+}
+
+void clip(std::string infile, std::string outfile, double top_left_x, double top_left_y, double size) {
+    GDALDatasetH source, dest;
+    GDALDriverH pDriver;
+    pDriver = GDALGetDriverByName("GTiff");
+
+    source = GDALOpen(infile.c_str(), GA_ReadOnly);
+
+    //the affine transformation information, you will need to adjust this to properly
+    //display the clipped raster
+    double transform[6];
+    GDALGetGeoTransform(source, transform);
+
+    std::cout << transform[0] << std::endl;
+    std::cout << transform[3] << std::endl;
+
+    //adjust top left coordinates
+    transform[0] = top_left_x;
+    transform[3] = top_left_y;
+
+    //determine dimensions of the new (cropped) raster in cells
+    int xSize = round(size / transform[1]);
+    int ySize = xSize;
+
+    //create the new (cropped) dataset
+    dest = GDALCreate(pDriver, outfile.c_str(), xSize, ySize,
+                        GDALGetRasterCount(source), GDT_Float32, NULL);
+
+    // Get Source coordinate system.
+    const char *pszDstWKT = NULL;
+
+    // Get Webmercator coordinate system
+    OGRSpatialReference oSRS;
+    oSRS.importFromEPSG(3857);
+    oSRS.exportToWkt(const_cast<char **>(&pszDstWKT));
+
+    GDALSetProjection(dest, pszDstWKT);
+    GDALSetGeoTransform(dest, transform);
+
+    // Copy the color table, if required.
+    GDALColorTableH hCT;
+    hCT = GDALGetRasterColorTable(GDALGetRasterBand(source, 1));
+    if (hCT != NULL)
+        GDALSetRasterColorTable(GDALGetRasterBand(dest, 1), hCT);
+
+    // FIXME: We reproject to get the data into the image. This is unnecessarily complex though, there must be a simpler way
+    // Setup warp options.
+    GDALWarpOptions *psWarpOptions = GDALCreateWarpOptions();
+    psWarpOptions->hSrcDS = source;
+    psWarpOptions->hDstDS = dest;
+    psWarpOptions->nBandCount = 1;
+    psWarpOptions->panSrcBands =
+            (int *) CPLMalloc(sizeof(int) * psWarpOptions->nBandCount);
+    psWarpOptions->panSrcBands[0] = 1;
+    psWarpOptions->panDstBands =
+            (int *) CPLMalloc(sizeof(int) * psWarpOptions->nBandCount);
+    psWarpOptions->panDstBands[0] = 1;
+    psWarpOptions->pfnProgress = GDALTermProgress;
+
+    // Establish reprojection transformer.
+    psWarpOptions->pTransformerArg =
+            GDALCreateGenImgProjTransformer(source,
+                                            GDALGetProjectionRef(source),
+                                            dest,
+                                            GDALGetProjectionRef(dest),
+                                            FALSE, 0.0, 1);
+    psWarpOptions->pfnTransformer = GDALGenImgProjTransform;
+
+    // Initialize and execute the warp operation.
+    GDALWarpOperation oOperation;
+    oOperation.Initialize(psWarpOptions);
+    oOperation.ChunkAndWarpImage(0, 0,
+                                 GDALGetRasterXSize(dest),
+                                 GDALGetRasterYSize(dest));
+    GDALDestroyGenImgProjTransformer(psWarpOptions->pTransformerArg);
+    GDALDestroyWarpOptions(psWarpOptions);
+
+    GDALClose(dest);
+    GDALClose(source);
+}
+
+int main() {
+    GDALAllRegister();
+
+    // reproject_to_webmercator("data/25m_EU_clip.tif", "data/25m_EU_clip_webm.tif");
+
+    float new_top_left_x = 1470287.0;
+    float new_top_left_y = 6013574.0;
+    float new_size = 5000.0;
+
+    clip("data/25m_EU_clip_webm.tif", "data/25m_EU_clip_webm_tile.tif", new_top_left_x, new_top_left_y, new_size);
 
     return 0;
 }
