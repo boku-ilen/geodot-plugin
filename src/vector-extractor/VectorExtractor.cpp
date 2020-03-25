00a051052f524b936a5de80725342022b5b3dcb4
Karl Bittner <karl.bittner@outlook.com>
2020-03-25 19:59:31 +0100

    Actually filter lines by the given parameters (position + radius)
    It seems like this is working as intended now! But it's not tested much.

--- src/vector-extractor/VectorExtractor.cpp
+++ src/vector-extractor/VectorExtractor.cpp
@@ -39,8 +39,38 @@ VectorExtractor::get_lines_near_position(const char *path, double pos_x, double 
     // TODO: Check poDS->GetLayerCount() to make sure there's exactly one layer? Or handle >1 layers too?
     OGRLayer *poLayer = poDS->GetLayers()[0];
 
-    list.emplace_back(new LineFeature(poLayer->GetNextFeature()));
-    list.emplace_back(new LineFeature(poLayer->GetNextFeature()));
+    // We want to extract the features within the circle constructed with the given position and radius from the vector layer.
+    // For this circle, we have to create a new dataset + layer + feature + geometry because layers can only be
+    //  intersected with other layers, and layers need a dataset.
+
+    // Create the circle geometry
+    OGRGeometry *circle = new OGRPoint(pos_x, pos_y);
+    OGRGeometry *circle_buffer = circle->Buffer(radius);
+
+    // Create the dataset in RAM
+    GDALDriver *out_driver = (GDALDriver *)GDALGetDriverByName("Memory");
+    GDALDataset *intersection_dataset = out_driver->Create("", 0, 0, 0, GDT_Unknown, nullptr);
+
+    // Create the layer for that dataset
+    OGRLayer *circle_layer = intersection_dataset->CreateLayer("IntersectionCircle");
+
+    // Create the feature for that layer
+    OGRFeature *circle_feature = OGRFeature::CreateFeature(circle_layer->GetLayerDefn());
+    circle_feature->SetGeometry(circle_buffer);
+    circle_layer->CreateFeature(circle_feature);
+
+    // Finally do the actual intersection, save the result to a new layer in the previously created dataset
+    OGRLayer *lines_within = intersection_dataset->CreateLayer("LinesWithinCircle");
+    poLayer->Intersection(circle_layer, lines_within);
+
+    // Put the resulting features into the returned list. We add as many features as were returned unless they're more
+    //  than the given max_amount.
+    int num_features = lines_within->GetFeatureCount();
+    int iterations = std::min(num_features, max_amount);
+
+    for (int i = 0; i < iterations; i++) {
+        list.emplace_back(new LineFeature(lines_within->GetNextFeature()));
+    }
 
     return list;
 }
--- src/vector-extractor/test.cpp
+++ src/vector-extractor/test.cpp
@@ -2,18 +2,21 @@
 #include "VectorExtractor.h"
 
 int main() {
-    LineFeature *line = VectorExtractor::get_lines_near_position("/home/karl/BOKU/geodata/streets/2_linknetz_ogd_WM.shp", 0.0, 0.0, 0.0, 10).front();
+    LineFeature *line = VectorExtractor::get_lines_near_position("/home/karl/BOKU/geodata/streets/2_linknetz_ogd_WM.shp", 1755382.5, 6156320.8, 1000.0, 10).front();
 
-    std::cout << "Length: " << line->get_attribute("LENGTH") << std::endl;
+    if (line) {
+        std::cout << "Length: " << line->get_attribute("LENGTH") << std::endl;
 
-    std::cout << "Points:" << std::endl;
+        std::cout << "Points:" << std::endl;
 
-    int point_count = line->get_point_count();
+        int point_count = line->get_point_count();
 
-    std::cout << point_count << " points" << std::endl;
-    
-    for (int i = 0; i < point_count; i++) {
-        std::cout << line->get_line_point_x(i) << ", " << line->get_line_point_y(i) << ", " << line->get_line_point_z(i) << std::endl;
+        std::cout << point_count << " points" << std::endl;
+
+        for (int i = 0; i < point_count; i++) {
+            std::cout << line->get_line_point_x(i) << ", " << line->get_line_point_y(i) << ", " << line->get_line_point_z(i)
+                      << std::endl;
+        }
     }
 
     return 0;
