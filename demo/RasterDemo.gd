47ce110ee91c0c7e41a5c7de764b1aa5f80cc78d
Karl Bittner <karl.bittner@outlook.com>
2020-03-26 11:49:24 +0100

    Expose variables for RasterDemo in editor

--- demo/RasterDemo.gd
+++ demo/RasterDemo.gd
@@ -1,5 +1,15 @@
 extends Spatial
 
+export(float) var start_position_webmercator_x = 1815513.0
+export(float) var start_position_webmercator_y = 6137496.0
+export(float) var tile_size_meters = 400.0
+export(int) var tile_size_pixels = 256
+
+export(String) var heightmap_data_path
+export(String) var heightmap_data_ending
+
+export(String) var ortho_data_path
+export(String) var ortho_data_ending
 
 var instanced_image_count = 0
 var time_passed = 0
@@ -9,21 +19,21 @@ func _process(delta):
 	time_passed += delta
 	
 	var img = Geodot.get_image(
-		"/media/karl/loda/geodata/wien-dhm-1m",
-		"tif",
-		1815513 + time_passed * 10.0,
-		6137496,
-		400.0,
-		256,
+		heightmap_data_path,
+		heightmap_data_ending,
+		start_position_webmercator_x + time_passed * 10.0,
+		start_position_webmercator_y,
+		tile_size_meters,
+		tile_size_pixels,
 		1
 	)
 	var ortho = Geodot.get_image(
-		"/media/karl/loda/geodata/wien-orthos-15cm",
-		"tif",
-		1815513 + time_passed * 10.0,
-		6137496,
-		400.0,
-		256,
+		ortho_data_path,
+		ortho_data_ending,
+		start_position_webmercator_x + time_passed * 10.0,
+		start_position_webmercator_y,
+		tile_size_meters,
+		tile_size_pixels,
 		1
 	)
 	
