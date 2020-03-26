7b92cf363807570cc71d8ea9aa39f4378d103abb
Karl Bittner <karl.bittner@outlook.com>
2020-03-26 12:31:09 +0100

    Document functions in VectorExtractor

--- src/vector-extractor/LineFeature.h
+++ src/vector-extractor/LineFeature.h
@@ -10,22 +10,29 @@ class OGRFeature;
 
 class OGRLineString;
 
+/// Wrapper for an OGRFeature representing a Line.
 class LineFeature {
 public:
     explicit LineFeature(OGRFeature *feature);
 
+    /// Return a map with all attribute names -> values.
     std::map<std::string, std::string> get_attributes();
 
+    /// Return the value of the attribute with the given name.
+    /// A field with the given name must exist.
     const char *get_attribute(const char *name);
 
+    /// Return the point in the line at the given index as a std::vector with 3 double entries (3D vector).
+    /// The index must be between 0 and get_point_count()-1.
     std::vector<double> get_line_point(int index);
 
+    /// Get the individual x, y and z components from the line point at the given index.
+    /// The same can be accomplished with the `get_line_point` function, but this one works without special std types.
     double get_line_point_x(int index);
-
     double get_line_point_y(int index);
-
     double get_line_point_z(int index);
 
+    /// Return the number of points in the line.
     int get_point_count();
 
 private:
--- src/vector-extractor/VectorExtractor.h
+++ src/vector-extractor/VectorExtractor.h
@@ -6,8 +6,14 @@
 
 class VectorExtractor {
 public:
+    /// Must be called before any other function to initialize GDAL.
     static void initialize();
 
+    /// Return a list of all LineFeatures in the dataset at the given path which are within the circle created by the
+    ///  given position and radius.
+    /// If there are more lines than the given max_amount within that circle, the last few are skipped.
+    /// TODO: Currently the lines which are skipped are chosen arbitrarily. It would be better to skip further away
+    ///  lines in this case.
     static std::list<LineFeature *>
     get_lines_near_position(const char *path, double pos_x, double pos_y, double radius, int max_amount);
 };
