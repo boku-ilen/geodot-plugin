4c54d9d1ec2589a3880c0b48f5bc65d3ef3d785e
karl <karl.bittner@hexaquo.at>
2020-07-24 13:17:26 +0200

    Expand class defitions in geodata.h
    Some internal functions and variables were added, as well as docstrings.
    
    The old functions were also deleted from geodot.h.

--- src/geodata.h
+++ src/geodata.h
@@ -6,6 +6,9 @@
 #include "defines.h"
 #include "geoimage.h"
 
+// Forward declaration of OGRLayer
+class OGRLayer;
+
 namespace godot {
 
 /// A dataset which contains layers of geodata.
@@ -24,9 +27,23 @@ public:
     /// Returns true if the GeoDataset could successfully be loaded.
     bool is_valid();
 
+    /// Returns a GeoRasterLayer object of the layer within this dataset with the given name.
+    /// It is recommended to check the validity of the returned object with GeoRasterLayer::is_valid().
     GeoRasterLayer get_raster_layer(String name);
 
+    /// Returns a GeoFeatureLayer object of the layer within this dataset with the given name.
+    /// It is recommended to check the validity of the returned object with GeoFeatureLayer::is_valid().
     GeoFeatureLayer get_feature_layer(String name);
+
+    /// Load a dataset file such as a Geopackage or a Shapefile into this object.
+    void load_from_file(String file_path);
+
+    /// Set the GDALDataset object directly.
+    /// Not exposed to Godot since Godot doesn't know about GDALDatasets - this is only for internal use.
+    void set_gdaldataset(GDALDataset *new_dataset);
+
+private:
+    GDALDataset *dataset;
 };
 
 /// A layer which contains any number of features.
@@ -47,13 +64,29 @@ public:
     /// Returns true if the layer could successfully be loaded.
     bool is_valid();
 
+    /// Returns all features, regardless of the geometry, wtihin this layer.
     Array get_all_features();
 
+    /// Returns features with line geometry near the given position (within the given radius).
     Array get_lines_near_position(double pos_x, double pos_y, double radius, int max_lines);
 
+    /// Returns features with point geometry near the given position (within the given radius).
     Array get_points_near_position(double pos_x, double pos_y, double radius, int max_points);
 
+    /// Crops features with line geometry to the square created by the given coordinates and size.
+    /// Useful for doing tile-based requests.
     Array crop_lines_to_square(double top_left_x, double top_left_y, double size_meters, int max_lines);
+
+    /// Load the first layer of the dataset at the given path into this object.
+    /// Useful e.g. for simple shapefiles with only one layer.
+    void load_from_file(String file_path);
+
+    /// Set the OGRLayer object directly.
+    /// Not exposed to Godot since Godot doesn't know about GDALDatasets - this is only for internal use.
+    void set_ogrlayer(OGRLayer *new_layer);
+
+private:
+    OGRLayer *layer;
 };
 
 /// A layer which contains raster data.
@@ -74,6 +107,16 @@ public:
 
     Ref<GeoImage> get_image(double top_left_x, double top_left_y, double size_meters,
                             int img_size, int interpolation_type);
+    
+    /// Load a raster dataset file such as a GeoTIFF into this object.
+    void load_from_file(String file_path);
+
+    /// Set the GDALDataset object for this layer. Must be a valid raster dataset.
+    /// Not exposed to Godot since Godot doesn't know about GDALDatasets - this is only for internal use.
+    void set_gdaldataset(GDALDataset *new_dataset);
+
+private:
+    GDALDataset *dataset;
 };
 
 }
--- src/geodot.h
+++ src/geodot.h
@@ -27,23 +27,7 @@ public:
     void _init();
     static void _register_methods();
 
-    /// Get a GeoImage from the geodata at the given path, with the given parameters.
-    /// The raster data at the given path should contain valid data for the given position and size.
-    /// If there is a directory named <path>.pyramid, it is assumed to be pre-tiled data, and it is returned.
-    ///  Note that for pre-tiled data, the position and size of the returned image can't exactly match the given position and size - the closest tile is chosen.
-    ///  The format follows the OSM standard: <path>.pyramid/<zoom>/<tile_x>/<tile_y>.<file_ending>
-    /// Otherwise, a georaster named <path>.<file_ending> is loaded and used.
-    Ref<GeoImage> get_image(String path, String file_ending,
-                            double top_left_x, double top_left_y, double size_meters,
-                            int img_size, int interpolation_type);
-
-    Array get_lines_near_position(String path, double pos_x, double pos_y, double radius, int max_lines);
-
-    Array get_points_near_position(String path, double pos_x, double pos_y, double radius, int max_points);
-
-    Array crop_lines_to_square(String path, double top_left_x, double top_left_y, double size_meters, int max_lines);
-
-    Array get_all_features(String path, String layer_name);
+    // TODO: Functions for getting a GeoDataset (maybe also raster layer?)
 
     Geodot();
     ~Geodot();
