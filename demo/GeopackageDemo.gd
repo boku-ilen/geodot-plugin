b83d4abf1e522b2f7d7efdc3ab9b2dfb8c6f166d
Clemens Tolboom <clemens@build2be.com>
2021-01-29 10:22:18 +0100

    Add info for easier debugging.

--- demo/GeopackageDemo.gd
+++ demo/GeopackageDemo.gd
@@ -8,10 +8,20 @@ export(String) var attribute_name
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("GeoDot info")
+	print(' path              : ', path)
+	print(' layer_name        : ', layer_name)
+	print(' attribute_name    : ', attribute_name)
+
+	print(' fetching dataset  : ', path)
 	var dataset = Geodot.get_dataset(path)
+
+	print(' fetching layer    : ', layer_name)
 	var layer = dataset.get_feature_layer(layer_name)
-	
+
+	print(' fetching features : ', layer_name)
 	var features = layer.get_all_features()
-	
+
+	print(' print features    : ', layer_name)
 	for feature in features:
 		print(feature.get_attribute(attribute_name))
