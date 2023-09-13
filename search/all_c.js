c8bfa616b5f6b05d8a6eaa9dabbe2d71fd6bd145
kb173 <kb173@users.noreply.github.com>
2023-09-13 09:41:24 +0000

    deploy: ac97908b80c5f347e61049fd78425286ee7ccf0b

--- search/all_c.js
+++ search/all_c.js
@@ -6,9 +6,10 @@ var searchData=
   ['set_5fnative_5fdataset_3',['set_native_dataset',['../classgodot_1_1GeoRasterLayer.html#af4ab9cb8096fe07f08c9fe246a0a3499',1,'godot::GeoRasterLayer::set_native_dataset()'],['../classgodot_1_1GeoDataset.html#afccdda02c07587f171302e2458938e06',1,'godot::GeoDataset::set_native_dataset()']]],
   ['set_5fnative_5flayer_4',['set_native_layer',['../classgodot_1_1GeoFeatureLayer.html#a15263c89edbf932f6dd4328db8a03b28',1,'godot::GeoFeatureLayer']]],
   ['set_5forigin_5fdataset_5',['set_origin_dataset',['../classgodot_1_1GeoFeatureLayer.html#a45f77fa3a58b280815e1a38b7ef159fb',1,'godot::GeoFeatureLayer::set_origin_dataset()'],['../classgodot_1_1GeoRasterLayer.html#aa259588fb7841da7e85bbe613c547f4b',1,'godot::GeoRasterLayer::set_origin_dataset()']]],
-  ['set_5fpoint_5fcount_6',['set_point_count',['../classLineFeature.html#ad26b65db0e3f785e5c793e2332235a77',1,'LineFeature']]],
-  ['set_5fraster_7',['set_raster',['../classgodot_1_1GeoImage.html#a949bd7f796ecc12511144fa9c511d259',1,'godot::GeoImage']]],
-  ['set_5fraster_5ffrom_5fband_8',['set_raster_from_band',['../classgodot_1_1GeoImage.html#ab1dfce450ef0c317b0b49f4a95f4051c',1,'godot::GeoImage']]],
-  ['set_5fvalue_5fat_5fposition_9',['set_value_at_position',['../classgodot_1_1GeoRasterLayer.html#ad26c39a468cba6337f50122daaeba009',1,'godot::GeoRasterLayer']]],
-  ['smooth_5fadd_5fvalue_5fat_5fposition_10',['smooth_add_value_at_position',['../classgodot_1_1GeoRasterLayer.html#aa3ba940845cb86d0fb8fd9285f4eb670',1,'godot::GeoRasterLayer']]]
+  ['set_5fouter_5fvertices_6',['set_outer_vertices',['../classPolygonFeature.html#a979369ce3f48649d3a9759d13ba62e25',1,'PolygonFeature']]],
+  ['set_5fpoint_5fcount_7',['set_point_count',['../classLineFeature.html#ad26b65db0e3f785e5c793e2332235a77',1,'LineFeature']]],
+  ['set_5fraster_8',['set_raster',['../classgodot_1_1GeoImage.html#a949bd7f796ecc12511144fa9c511d259',1,'godot::GeoImage']]],
+  ['set_5fraster_5ffrom_5fband_9',['set_raster_from_band',['../classgodot_1_1GeoImage.html#ab1dfce450ef0c317b0b49f4a95f4051c',1,'godot::GeoImage']]],
+  ['set_5fvalue_5fat_5fposition_10',['set_value_at_position',['../classgodot_1_1GeoRasterLayer.html#ad26c39a468cba6337f50122daaeba009',1,'godot::GeoRasterLayer']]],
+  ['smooth_5fadd_5fvalue_5fat_5fposition_11',['smooth_add_value_at_position',['../classgodot_1_1GeoRasterLayer.html#aa3ba940845cb86d0fb8fd9285f4eb670',1,'godot::GeoRasterLayer']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['pointfeature_0',['PointFeature',['../classPointFeature.html#a68d743c804d626d056192d5774da4143',1,'PointFeature::PointFeature(OGRFeature *feature)'],['../classPointFeature.html#ab507e1cbdaa246de031dac80edbecaf1',1,'PointFeature::PointFeature(OGRFeature *feature, OGRGeometry *ogrPoint)']]],
-  ['polygonfeature_1',['PolygonFeature',['../classPolygonFeature.html#a1119734e0a08edf02578c16fa6ba8f6e',1,'PolygonFeature::PolygonFeature(OGRFeature *feature)'],['../classPolygonFeature.html#ab3fea759b8d324530363f5f584ff6687',1,'PolygonFeature::PolygonFeature(OGRFeature *feature, const OGRGeometry *ogrPolygon)']]]
+  ['polygonfeature_1',['PolygonFeature',['../classPolygonFeature.html#a1119734e0a08edf02578c16fa6ba8f6e',1,'PolygonFeature::PolygonFeature(OGRFeature *feature)'],['../classPolygonFeature.html#a339b62245a9a5adae5da5219f06eddce',1,'PolygonFeature::PolygonFeature(OGRFeature *feature, OGRGeometry *ogrPolygon)']]]
 ];
