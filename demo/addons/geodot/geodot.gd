633513c54d165e921e49b568b28c77b8ec6f1d60
LandscapeLab Office <landscapelab@boku.ac.at>
2023-10-19 11:08:07 +0200

    feat: allow using Geodot in tool scripts
    With the @tool annotation, Geodot can be run in the editor. This is a requirement for using it in other tool scripts.

--- demo/addons/geodot/geodot.gd
+++ demo/addons/geodot/geodot.gd
@@ -1,14 +1,15 @@
+@tool
 extends Node
 
 
-func get_dataset(path: String, write_access=false) -> GeoDataset:
+static func get_dataset(path: String, write_access=false) -> GeoDataset:
 	var dataset := GeoDataset.new()
 	dataset.load_from_file(path, write_access)
 	
 	return dataset
 
 
-func get_raster_layer(path: String, write_access=false) -> GeoRasterLayer:
+static func get_raster_layer(path: String, write_access=false) -> GeoRasterLayer:
 	var layer := GeoRasterLayer.new()
 	layer.load_from_file(path, write_access)
 	
