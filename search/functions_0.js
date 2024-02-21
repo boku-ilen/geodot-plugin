3845ae6ec58212276b1004e3110cceb10f494e6f
kb173 <kb173@users.noreply.github.com>
2024-02-21 14:30:16 +0000

    deploy: 4a2dc7b3e2afe95ae37002c0a7ebbfd036b4a0e2

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['clone_0',['clone',['../classgodot_1_1GeoRasterLayer.html#ac118123e984167d0aefd22fabbc9bbfc',1,'godot::GeoRasterLayer']]],
-  ['create_5ffeature_1',['create_feature',['../classgodot_1_1GeoFeatureLayer.html#a47477798c09482c18d80cb99913aa830',1,'godot::GeoFeatureLayer::create_feature()'],['../classNativeLayer.html#a57efe65cd9b14d7b1d7505b7c849e513',1,'NativeLayer::create_feature()']]]
+  ['clear_5fcache_0',['clear_cache',['../classgodot_1_1GeoFeatureLayer.html#a01af602f041b65ea424f42211041731d',1,'godot::GeoFeatureLayer']]],
+  ['clear_5ffeature_5fcache_1',['clear_feature_cache',['../classNativeLayer.html#a5a5e9b5d6026303f9998be78f310f718',1,'NativeLayer']]],
+  ['clone_2',['clone',['../classgodot_1_1GeoRasterLayer.html#ac118123e984167d0aefd22fabbc9bbfc',1,'godot::GeoRasterLayer']]],
+  ['create_5ffeature_3',['create_feature',['../classgodot_1_1GeoFeatureLayer.html#a47477798c09482c18d80cb99913aa830',1,'godot::GeoFeatureLayer::create_feature()'],['../classNativeLayer.html#a57efe65cd9b14d7b1d7505b7c849e513',1,'NativeLayer::create_feature()']]]
 ];
