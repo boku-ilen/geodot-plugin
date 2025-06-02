0bc0099ccbe706ffae43bff395b7c2db42631285
kb173 <kb173@users.noreply.github.com>
2025-06-02 09:22:05 +0000

    deploy: f2b0731ae5bfcc7dbaa8eb8037b039d35714fc41

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['has_5fwrite_5faccess_0',['has_write_access',['../classgodot_1_1GeoRasterLayer.html#af7c0f96cc5f000e040775d67d75345d6',1,'godot::GeoRasterLayer::has_write_access()'],['../classgodot_1_1GeoDataset.html#a62ac6a6cad86d64c5865e5ade148536a',1,'godot::GeoDataset::has_write_access()']]]
+  ['has_5flayer_0',['has_layer',['../classNativeDataset.html#a472fc073c531507f83ce52771284263e',1,'NativeDataset']]],
+  ['has_5fwrite_5faccess_1',['has_write_access',['../classgodot_1_1GeoRasterLayer.html#af7c0f96cc5f000e040775d67d75345d6',1,'godot::GeoRasterLayer::has_write_access()'],['../classgodot_1_1GeoDataset.html#a62ac6a6cad86d64c5865e5ade148536a',1,'godot::GeoDataset::has_write_access()']]]
 ];
