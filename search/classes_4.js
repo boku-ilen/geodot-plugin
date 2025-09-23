cb91054ff1bacae0de2d63e947e21b194a8c6b4e
kb173 <kb173@users.noreply.github.com>
2025-09-23 09:25:37 +0000

    deploy: 095e21ccbdb18e7a9e1c67691f3cdd114ea51088

--- search/classes_4.js
+++ search/classes_4.js
@@ -1,13 +1,15 @@
 var searchData=
 [
   ['geodataset_0',['GeoDataset',['../classgodot_1_1GeoDataset.html',1,'godot']]],
-  ['geofeature_1',['GeoFeature',['../classgodot_1_1GeoFeature.html',1,'godot']]],
-  ['geofeaturelayer_2',['GeoFeatureLayer',['../classgodot_1_1GeoFeatureLayer.html',1,'godot']]],
-  ['geoimage_3',['GeoImage',['../classgodot_1_1GeoImage.html',1,'godot']]],
-  ['geoline_4',['GeoLine',['../classgodot_1_1GeoLine.html',1,'godot']]],
-  ['geopoint_5',['GeoPoint',['../classgodot_1_1GeoPoint.html',1,'godot']]],
-  ['geopolygon_6',['GeoPolygon',['../classgodot_1_1GeoPolygon.html',1,'godot']]],
-  ['georaster_7',['GeoRaster',['../classGeoRaster.html',1,'']]],
-  ['georasterlayer_8',['GeoRasterLayer',['../classgodot_1_1GeoRasterLayer.html',1,'godot']]],
-  ['geotransform_9',['GeoTransform',['../classgodot_1_1GeoTransform.html',1,'godot']]]
+  ['geodatasetloader_1',['GeoDatasetLoader',['../classgodot_1_1GeoDatasetLoader.html',1,'godot']]],
+  ['geofeature_2',['GeoFeature',['../classgodot_1_1GeoFeature.html',1,'godot']]],
+  ['geofeaturelayer_3',['GeoFeatureLayer',['../classgodot_1_1GeoFeatureLayer.html',1,'godot']]],
+  ['geoimage_4',['GeoImage',['../classgodot_1_1GeoImage.html',1,'godot']]],
+  ['geoline_5',['GeoLine',['../classgodot_1_1GeoLine.html',1,'godot']]],
+  ['geopoint_6',['GeoPoint',['../classgodot_1_1GeoPoint.html',1,'godot']]],
+  ['geopolygon_7',['GeoPolygon',['../classgodot_1_1GeoPolygon.html',1,'godot']]],
+  ['georaster_8',['GeoRaster',['../classGeoRaster.html',1,'']]],
+  ['georasterlayer_9',['GeoRasterLayer',['../classgodot_1_1GeoRasterLayer.html',1,'godot']]],
+  ['georasterlayerloader_10',['GeoRasterLayerLoader',['../classgodot_1_1GeoRasterLayerLoader.html',1,'godot']]],
+  ['geotransform_11',['GeoTransform',['../classgodot_1_1GeoTransform.html',1,'godot']]]
 ];
