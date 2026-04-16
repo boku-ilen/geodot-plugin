a1115276ffc8ced4773595805da7c826e8f357c7
kb173 <kb173@users.noreply.github.com>
2026-04-16 11:25:58 +0000

    deploy: 86492f5cff991229317dbbf4cc697f0e9e9ad5ec

--- search/functions_9.js
+++ search/functions_9.js
@@ -2,14 +2,15 @@ var searchData=
 [
   ['save_5fnew_0',['save_new',['../classgodot_1_1GeoFeatureLayer.html#aad9bac43f6b8af39943e1431abcd5dd0',1,'godot::GeoFeatureLayer']]],
   ['save_5foverride_1',['save_override',['../classgodot_1_1GeoFeatureLayer.html#a678ec54929db6e20af6bbb20804e2fcc',1,'godot::GeoFeatureLayer']]],
-  ['set_5fline_5fpoint_2',['set_line_point',['../classLineFeature.html#a13954c2e4b3db6375434610c327bfe13',1,'LineFeature']]],
-  ['set_5fnative_5fdataset_3',['set_native_dataset',['../classgodot_1_1GeoRasterLayer.html#af4ab9cb8096fe07f08c9fe246a0a3499',1,'godot::GeoRasterLayer::set_native_dataset()'],['../classgodot_1_1GeoDataset.html#afccdda02c07587f171302e2458938e06',1,'godot::GeoDataset::set_native_dataset()']]],
-  ['set_5fnative_5flayer_4',['set_native_layer',['../classgodot_1_1GeoFeatureLayer.html#a15263c89edbf932f6dd4328db8a03b28',1,'godot::GeoFeatureLayer']]],
-  ['set_5forigin_5fdataset_5',['set_origin_dataset',['../classgodot_1_1GeoFeatureLayer.html#a45f77fa3a58b280815e1a38b7ef159fb',1,'godot::GeoFeatureLayer::set_origin_dataset()'],['../classgodot_1_1GeoRasterLayer.html#aa259588fb7841da7e85bbe613c547f4b',1,'godot::GeoRasterLayer::set_origin_dataset()']]],
-  ['set_5fouter_5fvertices_6',['set_outer_vertices',['../classPolygonFeature.html#a979369ce3f48649d3a9759d13ba62e25',1,'PolygonFeature']]],
-  ['set_5fpoint_5fcount_7',['set_point_count',['../classLineFeature.html#ad26b65db0e3f785e5c793e2332235a77',1,'LineFeature']]],
-  ['set_5fraster_8',['set_raster',['../classgodot_1_1GeoImage.html#a949bd7f796ecc12511144fa9c511d259',1,'godot::GeoImage']]],
-  ['set_5fraster_5ffrom_5fband_9',['set_raster_from_band',['../classgodot_1_1GeoImage.html#ab1dfce450ef0c317b0b49f4a95f4051c',1,'godot::GeoImage']]],
-  ['set_5fvalue_5fat_5fposition_10',['set_value_at_position',['../classgodot_1_1GeoRasterLayer.html#ad26c39a468cba6337f50122daaeba009',1,'godot::GeoRasterLayer']]],
-  ['smooth_5fadd_5fvalue_5fat_5fposition_11',['smooth_add_value_at_position',['../classgodot_1_1GeoRasterLayer.html#aa3ba940845cb86d0fb8fd9285f4eb670',1,'godot::GeoRasterLayer']]]
+  ['set_5fbinary_5fattribute_2',['set_binary_attribute',['../classgodot_1_1GeoFeature.html#a1c43d2c11f3de16ce8556c8f95c5807d',1,'godot::GeoFeature']]],
+  ['set_5fline_5fpoint_3',['set_line_point',['../classLineFeature.html#a13954c2e4b3db6375434610c327bfe13',1,'LineFeature']]],
+  ['set_5fnative_5fdataset_4',['set_native_dataset',['../classgodot_1_1GeoRasterLayer.html#af4ab9cb8096fe07f08c9fe246a0a3499',1,'godot::GeoRasterLayer::set_native_dataset()'],['../classgodot_1_1GeoDataset.html#afccdda02c07587f171302e2458938e06',1,'godot::GeoDataset::set_native_dataset()']]],
+  ['set_5fnative_5flayer_5',['set_native_layer',['../classgodot_1_1GeoFeatureLayer.html#a15263c89edbf932f6dd4328db8a03b28',1,'godot::GeoFeatureLayer']]],
+  ['set_5forigin_5fdataset_6',['set_origin_dataset',['../classgodot_1_1GeoFeatureLayer.html#a45f77fa3a58b280815e1a38b7ef159fb',1,'godot::GeoFeatureLayer::set_origin_dataset()'],['../classgodot_1_1GeoRasterLayer.html#aa259588fb7841da7e85bbe613c547f4b',1,'godot::GeoRasterLayer::set_origin_dataset()']]],
+  ['set_5fouter_5fvertices_7',['set_outer_vertices',['../classPolygonFeature.html#a979369ce3f48649d3a9759d13ba62e25',1,'PolygonFeature']]],
+  ['set_5fpoint_5fcount_8',['set_point_count',['../classLineFeature.html#ad26b65db0e3f785e5c793e2332235a77',1,'LineFeature']]],
+  ['set_5fraster_9',['set_raster',['../classgodot_1_1GeoImage.html#a949bd7f796ecc12511144fa9c511d259',1,'godot::GeoImage']]],
+  ['set_5fraster_5ffrom_5fband_10',['set_raster_from_band',['../classgodot_1_1GeoImage.html#ab1dfce450ef0c317b0b49f4a95f4051c',1,'godot::GeoImage']]],
+  ['set_5fvalue_5fat_5fposition_11',['set_value_at_position',['../classgodot_1_1GeoRasterLayer.html#ad26c39a468cba6337f50122daaeba009',1,'godot::GeoRasterLayer']]],
+  ['smooth_5fadd_5fvalue_5fat_5fposition_12',['smooth_add_value_at_position',['../classgodot_1_1GeoRasterLayer.html#aa3ba940845cb86d0fb8fd9285f4eb670',1,'godot::GeoRasterLayer']]]
 ];
